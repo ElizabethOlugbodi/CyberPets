450d63d614aecba3ceb6e70bdc62d28a19868656
Elizabeth_O <96290780+ElizabethOlugbodi@users.noreply.github.com>
2022-03-16 19:50:15 +0000

    Add files via upload

---
+++ cyberPet.js
@@ -0,0 +1,86 @@
+class Dog extends Characters {
+    constructor(name, dob){
+        super (name, dob);
+    }
+    healthLevel = 75;
+    thirstLevel = 75;
+    hungerLevel = 50;
+    happinessLevel = 50;
+    feed() {
+        if(this.hungerLevel >= 75){
+            this.hungerLevel -= 5;
+            this.thirstLevel += 10;
+            this.happinessLevel -= 45;
+            this.healthLevel -= 45;
+            console.log(`${this.name} is looking very thin! Please take care of your pet! ${this.name}'s hunger levels of ${this.hungerLevel} has reduced their happiness levels to ${this.happinessLevel} and health levels to ${this.healthLevel} `);
+        } else if (this.hungerLevel >= 50) {
+            this.hungerLevel -= 20;
+            this.thirstLevel += 10;
+            this.happinessLevel += 5;
+            console.log(`${this.name} is still hungry! Perhaps consider feeding a bit more? Hunger levels reduced to ${this.hungerLevel}, thirst levels increased to ${this.thirstLevel} and happiness levels have increased to ${this.happinessLevel}`);
+        } else {
+            this.hungerLevel -= 25;
+            this.thirstLevel += 5;
+            this.happinessLevel += 10;
+            console.log(`${this.name} is well fed! Perhaps consider some exercise to burn off some of that energy? Hunger levels have reduced to ${this.hungerLevel}, thirst level increased to ${this.thirstLevel} and happiness levels have increased to ${this.happinessLevel}.`)
+        }
+    }
+    drink() {
+        if(this.thirstLevel >= 75) {
+            this.thirstLevel -= 10;
+            this.hungerLevel +=10;
+            this.happinessLevel -= 25;
+            this.healthLevel -= 25;
+            console.log(`${this.name}'s thirst levels are still ${this.thirstLevel}, please always provide a source of water! ${this.name}'s happiness levels have decreased to ${this.happinessLevel} and health levels have decreased to ${this.healthLevel}. ${this.name} drank so much they filled themselves up, missing lunch and increasing hunger levels to ${this.hungerLevel}`);
+        } else if (this.thirstLevel >= 50) {
+            this.thirstLevel -= 15;
+            this.hungerLevel += 5;
+            this.happinessLevel += 5;
+            console.log(`${this.name} is feeling refreshed, although you should consider making liquids more available! Thirst level reduced to ${this.thirstLevel}, happiness level has increased to ${this.happinessLevel} and hunger levels have increased to ${this.hungerLevel}`);
+        } else {
+            this.thirstLevel -= 17;
+            this.hungerLevel += 5;
+            this.happinessLevel += 5;
+            console.log(`${this.name} is feeling really refreshed! Thirst levels have reduced to ${this.thirstLevel}, happiness level has increased to ${this.happinessLevel} and hunger level has increased to ${this.hungerLevel}. Perhaps ${this.name} would like to play?`);
+        }
+    }
+    play() {
+        if (this.hungerLevel >= 75 || this.thirstLevel >= 75) {
+            console.log(`Your pets hunger level is ${this.hungerLevel} and thirst is ${this.thirstLevel} - Your pet is too weak to play!`);
+        } else if (this.happinessLevel <= 25) {
+            this.happinessLevel += 15;
+            this.healthLevel += 10
+            this.hungerLevel += 10;
+            this.thirstLevel += 5;
+            console.log(`${this.name} really enjoyed playing, however is growing quite hungry! Hunger level increased to ${this.hungerLevel}, thirst levels increased to ${this.thirstLevel}. `);
+        } else if (this.happinessLevel >= 50) {
+            this.happinessLevel += 20;
+            this.healthLevel += 10;
+            this.hungerLevel += 10;
+            this.thirstLevel += 5;
+            console.log(`${this.name} had a great time! You're forming a great bond. All of this playing has increased ${this.name}s happiness levels to ${this.happinessLevel} and increased hunger levels to ${this.hungerLevel} as well as thirst levels to ${this.thirstLevel}`);
+        } else {
+            this.happinessLevel += 25;
+            this.healthLevel += 10;
+            this.hungerLevel += 10;
+            this.thirstLevel += 5;
+            console.log(`${this.name} couldn't be happier! You're an amazing owner and ${this.name}'s happiness levels have increased to ${this.happinessLevel} and health levels have increased to ${this.healthLevel}. Hunger levels have increased to ${this.hungerLevel} and thirst levels have increased to ${this.thirstLevel}.`);
+        }
+    }
+    exercise() {
+        if (this.hungerLevel >= 75 || this.thirstLevel >= 75) {
+            console.log(`${this.name} is too weak to exercise! Please feed/provide water!`);
+        } else if (this.hungerLevel >= 50 || this.thirstLevel >= 50) {
+            this.healthLevel += 10;
+            this.hungerLevel += 5;
+            this.thirstLevel += 2;
+            this.happinessLevel += 10;
+            console.log(`${this.name} had a great time! Your happiness level has increased to ${this.happinessLevel} and grew healthier to ${this.healthLevel} health points!`);
+        } else { 
+           if (this.hungerLevel <= 25 && this.thirstLevel <= 25 && this.healthLevel >= 50) 
+            this.happinessLevel += 25;
+            console.log(`${this.name} is growing very attached to you. You're a great owner, ${this.name}s happiness levels have increased to ${this.happinessLevel}`);
+        }
+    }
+
+}
